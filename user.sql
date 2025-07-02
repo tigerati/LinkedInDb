c19c283c0a1ce0232f4811f71863c2602a18b886
tigerati <atiwatpanboonprung@gmail.com>
2025-07-02 17:19:30 +0700

    add create post for user

--- user.sql
+++ user.sql
@@ -168,3 +168,21 @@ EXCEPTION
         RETURN 'User already has this skill';
 END;
 $$ LANGUAGE plpgsql;
+
+create or replace function create_post(
+    author_id int,
+    content varchar,
+    media_url varchar,
+    visibility bool
+)
+returns void as
+$$
+BEGIN
+    insert into tbl_post(
+        author_id,
+        content,
+        media_url,
+        visibility)
+    values ($1, $2, $3, $4);
+END;
+$$ language plpgsql;
